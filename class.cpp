9bceaea8659d8b48dd99d2024638af69288ea935
Peter Master <petehoke@comcast.net>
2016-05-21 15:06:27 -0700

    first commit

---
+++ class.cpp
@@ -0,0 +1,14 @@
+// implements class.h
+// Peter Master, 5/21
+
+class Class
+{
+public:
+
+//units, lecture time, quarters/sessions offered, prerequisites, location
+
+private:
+	int *timesOffered;
+
+}
+
